51d727b5b212c22ca2fa6b95b9ebe2395b21b1ae
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-01-09 11:43:24 +0800

    refactor t_test

--- xpore/diffmod/statstest.py
+++ xpore/diffmod/statstest.py
@@ -1,6 +1,6 @@
 import scipy.stats
 import numpy as np
-from typing import List
+from typing import List, Dict, Tuple
 
 """
 This file implements some statistical tests, used both for prefiltering sites prior to GMM fitting to improve performance and for assigning significance after the fit.
@@ -13,27 +13,31 @@ It is called by worker threads in scripts/diffmod.py .
 # (idx, pos, kmer) -> {'y': y, 'x': x, 'r': r, 'condition_names': condition_names_dummies, 'run_names': run_names_dummies, 'y_condition_names': condition_labels, 'y_run_names': run_labels}
 # by default, in the call only the values are passed along
 
-def t_test(data: List) -> float:
-    labels = data['x'].astype(np.bool)
-    means = data['y']
-
+def _separate_conds(labels, means) -> Tuple[np.array, np.array]:
     # check for at most two conditions
     assert labels.shape[1] == 2
 
     # separate means by labels
-    cond1means = means[labels[:,0]]
-    cond2means = means[labels[:,1]]
-    #cond2means = means[np.logical_not(labels)]
+    c1means = means[labels[:,0]]
+    c2means = means[labels[:,1]]
+    #c2means = means[np.logical_not(labels)]
+
+    return c1means, c2means
+
+
+def t_test(data: Dict) -> Tuple[float, float]:
+    c1means, c2means = _separate_conds(data['x'].astype(np.bool), data['y'])
 
     # perform t test on data
     stats, _ = scipy.stats.ttest_ind(cond1means, cond2means)
 
     # compute two-sided significance. |labels| - 2 degrees of freedom
-    return scipy.stats.t.sf(np.abs(stats), len(cond1means)+len(cond2means)-2)*2
+    return scipy.stats.t.sf(np.abs(stats), len(c1means)+len(c2means)-2)*2
     # equivalent, but sf can be more precise
     #(1 - scipy.stats.t.cdf(abs(stat), df)) * 2
 
-def z_test(y1, y2, n1, n2):
+def z_test(data: Dict) -> Tuple[float, float]:
+    print(data['x'], data['y'])
     # two-tailed
     p1 = y1.mean()
     p2 = y2.mean()
